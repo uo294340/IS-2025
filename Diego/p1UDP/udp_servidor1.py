b2904951315f8601d6a592d446de4002f585cd27
uo293690 <uo293690@uniovi.es>
2025-09-24 14:57:34 +0000

    servidor

--- Diego/p1UDP/udp_servidor1.py
+++ Diego/p1UDP/udp_servidor1.py
@@ -1,21 +1,21 @@
 import socket
 import sys
 
-if len(sys.argv)2
-    print(Uso python udp_servidor1.py puertos)
+if len(sys.argv)>2:
+    print("Uso python udp_servidor1.py puertos")
     sys.exit(1)
-elif len(sys.argv)==1
+elif len(sys.argv)==1:
     puerto=9999
-else
+else:
     puerto=int(sys.argv[1])
 
 #socket udp
 
 servidor=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 servidor.bind(('localhost',puerto))
-print(fServidor UDP escuchando en el puerto {puerto})
+print(f"Servidor UDP escuchando en el puerto {puerto}")
 
-while True
+while True:
     datos,direccion=servidor.recvfrom(1024)
-    print(fRecibido {datos.decode()} de {direccion})
-    servidor.sendto(bACK,direccion)
+    print(f"Recibido {datos.decode()} de {direccion}")
+    servidor.sendto(b"ACK",direccion)
