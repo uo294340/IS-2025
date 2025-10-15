b75451fbd886b4facba1efeac0797a3234a7a2ec
uo293690 <uo293690@uniovi.es>
2025-10-15 14:25:34 +0000

    Ejercicio 1 acabado

---
+++ Diego/P2/lanzarej1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script para lanzar el contenedor Nginx con dos sitios configurados
+
+docker run --rm -d --network pruebas \
+  --name nginx \
+  -p 80:80 \
+  -p 81:81 \
+  -v $(pwd)/html:/usr/share/nginx/html \
+  -v $(pwd)/html2:/usr/share/nginx/html2 \
+  -v $(pwd)/sitios_nginx:/etc/nginx/conf.d \
+  nginx
