883279c8b002d01fe916abef0ff236f69f3ebd93
Diego <uo293690@uniovi.es>
2026-01-06 19:06:55 +0100

    Primer commit proyecto Amigos

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+
+}
+implementation("org.osmdroid:osmdroid-android:6.1.13")
+implementation("androidx.preference:preference-ktx:1.2.1")
