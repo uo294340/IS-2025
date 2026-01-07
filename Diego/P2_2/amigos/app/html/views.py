55f1b2283bd5ec2a2deb3c3de238d7bb6fa29f3c
uo293690 <uo293690@uniovi.es>
2026-01-07 20:28:23 +0000

    Eliminar amigos

--- Diego/P2_2/amigos/app/html/views.py
+++ Diego/P2_2/amigos/app/html/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from . import html
 from ..models import Amigo
+from .. import db
 
 @html.route("/amigos")
 def tabla_amigos():
@@ -11,3 +12,19 @@ def tabla_amigos():
     amigos = Amigo.query.all()
     return render_template("tabla_amigos.html",
                            amigos=amigos)
+
+@html.route("/delete_amigo/<int:id>")
+def delete_amigo(id):
+    """
+    Borra un amigo de la base de datos
+    """
+    # El método get_or_404 proporcionado por SQLAlchemy
+    # se ocupa de generar un error 404 si el id no está en
+    # la base de datos
+    amigo = Amigo.query.get_or_404(id)
+    # Una vez obtenido, lo borramos
+    db.session.delete(amigo)
+    db.session.commit()
+
+    # Y redireccionamos a la vista /amigos
+    return redirect(url_for('html.tabla_amigos'))
