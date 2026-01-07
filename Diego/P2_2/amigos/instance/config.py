d2f3bfd23d5d55808b211ed12e8f9a3f7b04910b
uo293690 <uo293690@uniovi.es>
2026-01-07 20:15:32 +0000

    Primer commit sesion 2.2

---
+++ Diego/P2_2/amigos/instance/config.py
@@ -0,0 +1,2 @@
+# Definición de secretos a usar por la app
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://amigosuser:amigospass@172.19.0.2/amigosdb"
---
+++ Diego/P2_2/amigos/lanzar.py
@@ -0,0 +1,5 @@
+# Importar la función create_app desde la carpeta app
+from app import create_app
+
+# Usarlo para instanciar la aplicacion
+app = create_app("development")
