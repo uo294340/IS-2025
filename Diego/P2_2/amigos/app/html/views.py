c4d6a6082fced8ca8c2f6e03126a5b77f7c05923
uo293690 <uo293690@uniovi.es>
2026-01-07 20:38:54 +0000

    Finalizacion segunda version

--- Diego/P2_2/amigos/app/html/views.py
+++ Diego/P2_2/amigos/app/html/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, abort
 from . import html
 from ..models import Amigo
 from .. import db
@@ -28,3 +28,58 @@ def delete_amigo(id):
 
     # Y redireccionamos a la vista /amigos
     return redirect(url_for('html.tabla_amigos'))
+
+@html.route("/edit_amigo/<int:id>")
+def edit_amigo(id):
+    """
+    Presenta un formulario para obtener datos a modificar de un amigo
+    """
+    amigo = Amigo.query.get_or_404(id)
+    # Rellenar el template con los datos de este amigo y presentar
+    # el formulario al usuario
+    return render_template("edit_amigo.html", amigo=amigo)
+
+@html.route("/new_amigo/")
+def new_amigo():
+    """
+    Presenta un formulario para obtener datos para crear nuevo amigo
+    """
+    # No tenemos datos de ningún amigo previo
+    # Pero podemos usar el mismo template
+    return render_template("edit_amigo.html", amigo=None)
+
+@html.route("/save_amigo", methods=["POST"])
+def save_amigo():
+    id = request.form.get("id")
+    if id is None or id == "":
+        # En este caso se trata de un amigo nuevo, lo creamos
+        name = request.form.get("name")
+        if not name:
+            # Si falta el nombre, devolvemos un error HTTP 422
+            abort(422)
+        # Para lati y longi permitimos que estén ausentes, y en ese caso
+        # les damos un valor por defecto de "0"
+        lati = request.form.get("lati", "0")
+        longi = request.form.get("longi", "0")
+
+        # Creamos el amigo y lo añadimos a la base de datos
+        amigo = Amigo(name=name, lati=lati, longi=longi)
+        db.session.add(amigo)
+        db.session.commit()
+    else:
+        # En este caso se trata de un amigo de la base de datos
+        amigo = Amigo.query.get_or_404(int(id))
+        # Modificamos los campos que vengan en el formulario
+        name = request.form.get("name")
+        if name:
+            amigo.name = name
+        lati = request.form.get("lati")
+        if lati:
+            amigo.lati = lati
+        longi = request.form.get("longi", "0")
+        if longi:
+            amigo.longi = longi
+        # Una vez modificado, lo guardamos a la base de datos
+        db.session.commit()
+    # Redireccionamos hacia la tabla-lista de amigos
+    return redirect(url_for("html.tabla_amigos"))
